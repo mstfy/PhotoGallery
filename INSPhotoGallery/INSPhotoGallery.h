f2161911e9de0914c72f992d011e9c96af8a5902
Mustafa Yusuf <mustafa.yusuf@turkcell.com.tr>
2017-10-23 18:02:23 +0300

    Initial Commit

---
+++ INSPhotoGallery/INSPhotoGallery.h
@@ -0,0 +1,19 @@
+//
+//  INSPhotoGallery.h
+//  INSPhotoGallery
+//
+//  Created by Mustafa Yusuf on 23.10.2017.
+//  Copyright © 2017 Mustafa Yusuf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for INSPhotoGallery.
+FOUNDATION_EXPORT double INSPhotoGalleryVersionNumber;
+
+//! Project version string for INSPhotoGallery.
+FOUNDATION_EXPORT const unsigned char INSPhotoGalleryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <INSPhotoGallery/PublicHeader.h>
+
+
